f5209c0d5b9c57dea2573931933aadeb24a87002
Brenda Matias <brenda.matias@thoughtworks.com>
2021-05-03 18:49:22 -0300

    add component will unmount

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -5,11 +5,17 @@ class ListaDeCategorias extends Component {
     constructor() {
         super();
         this.state = { categorias: [] }
-    }
-    componentDidMount() {
-        this.props.categorias.inscrever(this._novasCategorias.bind(this));
+        this._novasCategorias = this._novasCategorias.bind(this);
     }
 
+    componentDidMount() {
+        this.props.categorias.inscrever(this._novasCategorias);
+    }
+
+    componentWillUnmount() {
+        this.props.categorias.desinscrever(this._novasCategorias);
+    }
+    
     _novasCategorias(categorias) {
         this.setState({...this.state, categorias})
     }
