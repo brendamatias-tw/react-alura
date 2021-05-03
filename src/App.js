ee5469b3211ef946f0d2a954448eeae0b22b243b
Brenda Matias <brenda.matias@thoughtworks.com>
2021-05-03 16:22:48 -0300

    add categories list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      notas:[]
+      notas: [],
+      categorias: [],
     }
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
     const novoEstado = {
-      notas:novoArrayNotas
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas
+    arrayNotas.splice(index, 1)
+
+    this.setState(arrayNotas)
+  }
+
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
+    const novoEstado = {
+      ...this.state,
+      categorias: novoArrayCategorias
     }
     this.setState(novoEstado)
   }
@@ -25,8 +43,17 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro
+          categorias={this.state.categorias}
+          criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.state.categorias} />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas} />
+        </main>
       </section>
     );
   }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,27 +2,26 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.titulo ="";
-    this.texto ="";
+    this.titulo = "";
+    this.texto = "";
   }
 
-  _handleMudancaTitulo(evento){
+  _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handleMudancaTexto(evento){
+  _handleMudancaTexto(evento) {
     evento.stopPropagation();
     this.texto = evento.target.value;
   }
 
-  _criarNota(evento){
+  _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
     this.props.criarNota(this.titulo, this.texto);
-    
   }
 
   render() {
@@ -30,6 +29,11 @@ class FormularioCadastro extends Component {
       <form className="form-cadastro"
         onSubmit={this._criarNota.bind(this)}
       >
+        <select className="form-cadastro_input">
+          {this.props.categorias.map((categoria) => {
+            return <option>{categoria}</option>
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
